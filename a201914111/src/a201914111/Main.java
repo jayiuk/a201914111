889d82f83f043067d572e974431505cf014119b2
Jeon YoungUk <jayiuk987@gmail.com>
2023-09-20 09:48:35 +0900

    two

--- a201914111/src/a201914111/Main.java
+++ a201914111/src/a201914111/Main.java
@@ -2,6 +2,6 @@ package a201914111;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("one 201914111");
+		System.out.println("one");
 	}
 }
---
+++ a201914111/src/a201914111/Two.java
@@ -0,0 +1,7 @@
+package a201914111;
+
+public class Two {
+    public static void main(String[] args){
+        System.out.println("two");
+    }
+}
